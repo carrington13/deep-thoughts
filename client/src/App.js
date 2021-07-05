00e2a2c1b27c2bb2c11b8cbd91389969dc94befa
Mediogre <caseyarrington13@gmail.com>
2021-07-05 09:43:27 -0500

    add Link and built out other apps

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import SingleThought from './pages/SingleThought'
 
 
 // creates link to the graphql
@@ -20,13 +25,24 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/signup' component={Signup} />
+              <Route exact path='/login' component={Login} />
+              {/* ? in profile/:username? means the parameter is optional */}
+              <Route exact path='/profile/:username?' component={Profile} />
+              <Route exact path='/thought/:id' component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
